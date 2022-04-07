d45d9f87d5fa9ce4a86c0878b1afdfc5ea417aa1
Seungin Yang <rhfpdk92@naver.com>
2022-04-07 23:21:01 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (6.47%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -14,15 +14,29 @@ class Solution {
             return head;
         }
 
-        ListNode first = head;
+        ListNode dummy = new ListNode(0);
+        ListNode res = dummy;
 
-        while (first != null && first.next != null) {
-            int tmp = first.val;
-            first.val = first.next.val;
-            first.next.val = tmp;
-            first = first.next.next;
+        ListNode n1 = head;
+        ListNode n2 = head.next;
+
+//    A - B -  C - D
+        while (n1 != null && n2 != null) {
+            res.next = n2;
+            ListNode tmp = n2.next;
+            n2.next = n1;
+            res = n1;
+
+            n1.next = tmp;
+            n1 = n1.next;
+
+            if (tmp != null) {
+                n2 = tmp.next;
+            }
+
+
         }
 
-        return head;
+        return dummy.next;
     }
 }
