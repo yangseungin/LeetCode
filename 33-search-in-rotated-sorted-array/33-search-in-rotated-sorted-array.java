b03eb53e02a5c725225b79e7b1efc0c5058d61e0
Seungin Yang <rhfpdk92@naver.com>
2022-04-17 22:30:31 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (86.70%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                if (nums[mid] > nums[right] && nums[right] >= target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            } else {
+                if (nums[mid] < nums[left] && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+        }
+        return -1;
+    }
+}
