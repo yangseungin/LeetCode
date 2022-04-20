7a64ea89a895e722a7178a62b5857fb301406bf3
Seungin Yang <rhfpdk92@naver.com>
2022-04-20 22:40:13 +0900

    Time: 4 ms (59.07%), Space: 126.6 MB (67.29%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,18 +1,19 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if(num>0) {
-                set.add(num);
+         boolean[] arr = new boolean[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0 && nums[i] <= nums.length) {
+                arr[nums[i]] = true;
             }
         }
-        for (int i = 1; i <= set.size(); i++) {
-            if(!set.contains(i)){
+        for (int i = 1; i < arr.length; i++) {
+            if (!arr[i]) {
                 return i;
             }
         }
 
+        return arr.length;
 
-        return set.size()+1;
     }
 }
