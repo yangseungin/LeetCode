f7f8b4799ef1abc2277ffde8f242eb9bf9174b27
Seungin Yang <rhfpdk92@naver.com>
2022-08-18 22:15:46 +0900

    Time: 8 ms (10.71%), Space: 80.5 MB (54.68%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,10 +1,14 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int index = 0, sumGas = 0, sumCost = 0, gasTank = 0;
+        int sumCost = Arrays.stream(cost).sum();
+        int sumGas = Arrays.stream(gas).sum();
+        if (sumCost > sumGas) {
+            return -1;
+        }
+
+        int index = 0, gasTank = 0;
 
         for (int i = 0; i < gas.length; i++) {
-            sumGas += gas[i];
-            sumCost += cost[i];
             gasTank += gas[i] - cost[i];
             if (gasTank < 0) {
                 gasTank = 0;
